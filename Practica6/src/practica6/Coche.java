c026b6767c955adbef4734da12231f9100c40e0d
JoseManuelRuizLopez <Josemanuelruizlopez97@gmail.com>
2019-05-15 19:29:32 +0200

    Metodos documentados en Javadoc. Closes #6.

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -1,5 +1,12 @@
 package practica6;
 
+/** 
+ * Clase Coche en donde se definen los atributos de Coche y son implementados los métodos combustible, cocheMasMatricula, 
+ * metodoGasolina, metodoDiesel, metodoHibrido.
+ *
+ *@version 2019
+ * @author José Manuel Ruiz López
+ */
 public class Coche 
 {
 	private static final String EL_COCHE_CON_MATRICULA = "El coche con matrícula ";
@@ -24,6 +31,9 @@ public class Coche
 		fabricante = f;
 	}
 
+	/**
+	 * @return El resultado es la información del coche según el tipo de combustible.
+	 */
 	public String combustible() 
 	{
 		String resultado = "";
@@ -49,22 +59,39 @@ public class Coche
 		return resultado;
 	}
 
+	/**
+	 * @return Devuelve un String con la cadena mas la matricula del coche.
+	 */
 	public String cocheMasMatricula()
 	{
 		return EL_COCHE_CON_MATRICULA + matricula + " ";
 	}
 	
-
+	/**
+	 * @param modeloCoche Atributo que es el modelo de coche.
+	 * @param fabricanteCoche Atributo que es el fabricante del coche.
+	 * @return Devuelve un String con el modelo y fabricante con el consumo euros por litros de la gasolína.
+	 */
 	public String metodoGasolina(String modeloCoche, String fabricanteCoche)
 	{
 		return "Es un " + fabricanteCoche + modeloCoche + " y gasta 1,337 euros por litro.";
 	}
 
+	/**
+	 * @param modeloCoche Atributo que es el modelo de coche.
+	 * @param fabricanteCoche Atributo que es el fabricante del coche.
+	 * @return Devuelve un String con el modelo y fabricante con el consumo euros por litros de el Diésel.
+	 */
 	public String metodoDiesel(String modeloCoche, String fabricanteCoche)
 	{
 		return "Es un " + fabricanteCoche + modeloCoche + " y gasta 1,052 euros por litro.";
 	}
 
+	/**
+	 * @param modeloCoche Atributo que es el modelo de coche.
+	 * @param fabricanteCoche Atributo que es el fabricante del coche.
+	 * @return Devuelve un String con el modelo y fabricante explicando que no necesita combustible.
+	 */
 	public String metodoHibrido(String modeloCoche, String fabricanteCoche)
 	{
 		return "Es un " + fabricanteCoche + modeloCoche +  " y no necesita combustible.";
